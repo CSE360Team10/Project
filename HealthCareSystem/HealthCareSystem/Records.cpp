081e7cb8a22657caaad6b5cf75c66452111c91d0
dj0wns <derekjones@asu.edu>
2015-04-19 21:55:39 -0700

    SurveyResults Class Skeleton

--- HealthCareSystem/HealthCareSystem/Records.cpp
+++ HealthCareSystem/HealthCareSystem/Records.cpp
@@ -3,3 +3,59 @@
 Records::Records(){
 	//ctor
 }
+
+std::queue<Patient> Records::getPatients(){
+	return patientRecords;
+}
+
+
+std::queue<Doctor> Records::getDoctors(){
+	return doctorRecords;
+}
+
+std::queue<SurveyResults> Records::getActiveSurveys(){
+	return activeSurveys;
+}
+
+bool Records::addPatient(Patient patient){
+	//todo
+	return 1;
+}
+
+bool Records::addDoctor(Doctor doctor){
+	//todo
+	return 1;
+}
+
+bool Records::addSurvey(SurveyResults survey){
+	//todo
+	return 1;
+}
+
+bool Records::removePatient(Patient patient){
+	//todo
+	return 1;
+}
+
+bool Records::removeDoctor(Doctor doctor){
+	//todo
+	return 1;
+}
+
+bool Records::removeSurvey(SurveyResults survey){
+	//todo
+	return 1;
+}
+
+Account search(std::string firstName, std::string lastName){
+
+	Account foo;
+	//TODO
+	return foo;
+}
+
+Account search(std::string fusername){
+	Account foo;
+	//TODO
+	return foo;
+}
