52ed0779818ca3863b8eaf957bc9dbc4d6a1488f
aboyan08 <anfu08@gmail.com>
2015-04-19 21:16:24 -0700

    Add Doctor

---
+++ HealthCareSystem/HealthCareSystem/Doctor.cpp
@@ -0,0 +1,2 @@
+// Doctor.cpp : Defines the doctor class.
+//
---
+++ HealthCareSystem/HealthCareSystem/Doctor.h
@@ -0,0 +1,15 @@
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+#include "stdafx.h"
+#include "Account.h"
+
+class Doctor: public Account
+{
+	public:
+		Doctor(); //CTOR
+	protected:
+	private:
+};
+
+#endif 
